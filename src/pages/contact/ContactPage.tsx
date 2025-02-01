0838d7862b1a42ac9d75081c83bf7fc4e1bfc1f2
mauvepark <nooraliyyc@gmail.com>
2025-02-01 12:16:38 -0700

    contact page

--- src/pages/contact/ContactPage.tsx
+++ src/pages/contact/ContactPage.tsx
@@ -11,6 +11,48 @@ const ContactPage = () => {
       <Navbar />
       <div className={styles.container}>
         <div className={styles.pageHeading}>Contact Us</div>
+        <div className={styles.descriptionContainer}>
+          <div>
+            <form id="contact-form" method="POST" onSubmit="">
+              <div>
+                <label id="name">Name</label>
+                <input
+                  className={styles.formInput}
+                  placeholder="Value"
+                  type="text"
+                />
+              </div>
+              <div>
+                <label id="surname">Surname</label>
+                <input
+                  className={styles.formInput}
+                  placeholder="Value"
+                  type="text"
+                />
+              </div>
+              <div>
+                <label id="inputEmail">Email address</label>
+                <input
+                  className={styles.formInput}
+                  placeholder="Value"
+                  type="email"
+                />
+              </div>
+              <div>
+                <label id="message">Message</label>
+                <textarea
+                  className={styles.formInput}
+                  placeholder="Value"
+                ></textarea>
+              </div>
+              <div>
+                <button className={styles.submitButton} type="submit">
+                  Submit
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
         <Image
           alt="backsplash"
           fill
