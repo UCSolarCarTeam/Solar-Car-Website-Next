17b8223656a2aa6c6670e0f4d9d96e6663953cfb
burtonjong <burtonjong05@gmail.com>
2025-03-11 19:25:29 -0600

    temp fix

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -14,6 +14,9 @@ const metadata: Metadata = {
 
 const Team = async () => {
   const teamHierarchy = await trpc.fe.getTeamMembers();
+
+  const tempFlag = false;
+
   const {
     accountingTeam,
     businessTeamManager,
@@ -43,117 +46,126 @@ const Team = async () => {
           working on our sixth car and have completed 7 races with previous
           vehicles.`}
           </div>
-          <div className={styles.teamMembersContainer}>
-            <div className={styles.teamManagers}>
-              <TeamMember user={engineeringTeamManager} />
-              <TeamMember user={teamCaptain} />
-              <TeamMember user={businessTeamManager} />
+          {tempFlag ? (
+            <div className={styles.teamMembersContainer}>
+              <div className={styles.teamManagers}>
+                <TeamMember user={engineeringTeamManager} />
+                <TeamMember user={teamCaptain} />
+                <TeamMember user={businessTeamManager} />
+              </div>
+              {leadRoles && leadRoles.length > 0 && (
+                <div className={styles.teamRoleContainer}>
+                  <div className={styles.title}>Team Leads</div>
+                  <div className={styles.teamMembers}>
+                    {leadRoles.map((teamMember) => (
+                      <TeamMember
+                        key={teamMember.clerkUserId}
+                        user={teamMember}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+              {accountingTeam && accountingTeam.length > 0 && (
+                <div className={styles.teamRoleContainer}>
+                  <div className={styles.title}>Accounting Team</div>
+                  <div className={styles.teamMembers}>
+                    {accountingTeam.map((teamMember) => (
+                      <TeamMember
+                        key={teamMember.clerkUserId}
+                        user={teamMember}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+              {commmunicationsTeam && commmunicationsTeam.length > 0 && (
+                <div className={styles.teamRoleContainer}>
+                  <div className={styles.title}>Communications Team</div>
+                  <div className={styles.teamMembers}>
+                    {commmunicationsTeam.map((teamMember) => (
+                      <TeamMember
+                        key={teamMember.clerkUserId}
+                        user={teamMember}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+              {sponsorshipTeam && sponsorshipTeam.length > 0 && (
+                <div className={styles.teamRoleContainer}>
+                  <div className={styles.title}>Sponsorship Team</div>
+                  <div className={styles.teamMembers}>
+                    {sponsorshipTeam.map((teamMember) => (
+                      <TeamMember
+                        key={teamMember.clerkUserId}
+                        user={teamMember}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+              {softwareTeam && softwareTeam.length > 0 && (
+                <div className={styles.teamRoleContainer}>
+                  <div className={styles.title}>Software Team</div>
+                  <div className={styles.teamMembers}>
+                    {softwareTeam.map((teamMember) => (
+                      <TeamMember
+                        key={teamMember.clerkUserId}
+                        user={teamMember}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+              {electricalTeam && electricalTeam.length > 0 && (
+                <div className={styles.teamRoleContainer}>
+                  <div className={styles.title}>Electrical Team</div>
+                  <div className={styles.teamMembers}>
+                    {electricalTeam.map((teamMember) => (
+                      <TeamMember
+                        key={teamMember.clerkUserId}
+                        user={teamMember}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+              {mechanicalTeam && mechanicalTeam.length > 0 && (
+                <div className={styles.teamRoleContainer}>
+                  <div className={styles.title}>Mechanical Team</div>
+                  <div className={styles.teamMembers}>
+                    {mechanicalTeam.map((teamMember) => (
+                      <TeamMember
+                        key={teamMember.clerkUserId}
+                        user={teamMember}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+              {multiTeam && multiTeam.length > 0 && (
+                <div className={styles.teamRoleContainer}>
+                  <div className={styles.title}>Multi Team</div>
+                  <div className={styles.teamMembers}>
+                    {multiTeam.map((teamMember) => (
+                      <TeamMember
+                        key={teamMember.clerkUserId}
+                        user={teamMember}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
             </div>
-            {leadRoles && leadRoles.length > 0 && (
-              <div className={styles.teamRoleContainer}>
-                <div className={styles.title}>Team Leads</div>
-                <div className={styles.teamMembers}>
-                  {leadRoles.map((teamMember) => (
-                    <TeamMember
-                      key={teamMember.clerkUserId}
-                      user={teamMember}
-                    />
-                  ))}
-                </div>
+          ) : (
+            <div className={styles.teamMembersContainer}>
+              <div className={styles.description}>
+                {`This page is currently being built. Please check back later for
+                more information.`}
               </div>
-            )}
-            {accountingTeam && accountingTeam.length > 0 && (
-              <div className={styles.teamRoleContainer}>
-                <div className={styles.title}>Accounting Team</div>
-                <div className={styles.teamMembers}>
-                  {accountingTeam.map((teamMember) => (
-                    <TeamMember
-                      key={teamMember.clerkUserId}
-                      user={teamMember}
-                    />
-                  ))}
-                </div>
-              </div>
-            )}
-            {commmunicationsTeam && commmunicationsTeam.length > 0 && (
-              <div className={styles.teamRoleContainer}>
-                <div className={styles.title}>Communications Team</div>
-                <div className={styles.teamMembers}>
-                  {commmunicationsTeam.map((teamMember) => (
-                    <TeamMember
-                      key={teamMember.clerkUserId}
-                      user={teamMember}
-                    />
-                  ))}
-                </div>
-              </div>
-            )}
-            {sponsorshipTeam && sponsorshipTeam.length > 0 && (
-              <div className={styles.teamRoleContainer}>
-                <div className={styles.title}>Sponsorship Team</div>
-                <div className={styles.teamMembers}>
-                  {sponsorshipTeam.map((teamMember) => (
-                    <TeamMember
-                      key={teamMember.clerkUserId}
-                      user={teamMember}
-                    />
-                  ))}
-                </div>
-              </div>
-            )}
-            {softwareTeam && softwareTeam.length > 0 && (
-              <div className={styles.teamRoleContainer}>
-                <div className={styles.title}>Software Team</div>
-                <div className={styles.teamMembers}>
-                  {softwareTeam.map((teamMember) => (
-                    <TeamMember
-                      key={teamMember.clerkUserId}
-                      user={teamMember}
-                    />
-                  ))}
-                </div>
-              </div>
-            )}
-            {electricalTeam && electricalTeam.length > 0 && (
-              <div className={styles.teamRoleContainer}>
-                <div className={styles.title}>Electrical Team</div>
-                <div className={styles.teamMembers}>
-                  {electricalTeam.map((teamMember) => (
-                    <TeamMember
-                      key={teamMember.clerkUserId}
-                      user={teamMember}
-                    />
-                  ))}
-                </div>
-              </div>
-            )}
-            {mechanicalTeam && mechanicalTeam.length > 0 && (
-              <div className={styles.teamRoleContainer}>
-                <div className={styles.title}>Mechanical Team</div>
-                <div className={styles.teamMembers}>
-                  {mechanicalTeam.map((teamMember) => (
-                    <TeamMember
-                      key={teamMember.clerkUserId}
-                      user={teamMember}
-                    />
-                  ))}
-                </div>
-              </div>
-            )}
-            {multiTeam && multiTeam.length > 0 && (
-              <div className={styles.teamRoleContainer}>
-                <div className={styles.title}>Multi Team</div>
-                <div className={styles.teamMembers}>
-                  {multiTeam.map((teamMember) => (
-                    <TeamMember
-                      key={teamMember.clerkUserId}
-                      user={teamMember}
-                    />
-                  ))}
-                </div>
-              </div>
-            )}
-          </div>
+            </div>
+          )}
         </div>
       </main>
       <Footer />
