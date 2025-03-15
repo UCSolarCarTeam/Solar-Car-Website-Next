f5f158e906209a8c47926b586f61edd3e8789950
brian-ngyn <brian.nguy1en@gmail.com>
2025-03-15 15:21:38 -0600

    fix: image compress harder

--- src/app/_lib/compress.ts
+++ src/app/_lib/compress.ts
@@ -1,8 +1,9 @@
-import imageCompression from "browser-image-compression";
+import imageCompression, { type Options } from "browser-image-compression";
 
 async function compress(file: File) {
   // Run some compression algorithm on the file
-  const options = {
+  const options: Options = {
+    maxSizeMB: 4,
     maxWidthOrHeight: 1600, // PIXELS
     useWebWorker: true,
   };
