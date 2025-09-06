dd5f0f5714ee797b2d9d92378ddd9e575ab4ac08
burtonjong <burtonjong05@gmail.com>
2025-09-06 11:36:43 -0600

    Revert "feat: only allow signin for invites"
    This reverts commit b1f992f1ac621643edd6efeaa4425de74dfe91d9.

--- src/app/portal/sign-in/[[...slug]]/page.tsx
+++ src/app/portal/sign-in/[[...slug]]/page.tsx
@@ -3,7 +3,7 @@ import { memo } from "react";
 import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
-  return <SignIn />;
+  return <SignIn withSignUp />;
 };
 
 export default memo(SignInPage);
