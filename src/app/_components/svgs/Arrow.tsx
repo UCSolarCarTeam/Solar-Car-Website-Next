79aa838305b6f49b0c790644aff85648009e398f
sim <simarkandola51@gmail.com>
2025-10-04 16:03:05 -0600

    fix lint errs

--- src/app/_components/svgs/Arrow.tsx
+++ src/app/_components/svgs/Arrow.tsx
@@ -9,26 +9,26 @@ const Arrow: React.FC<ArrowProps> = ({ direction, size = 40 }) => {
   const isLeft = direction === "left";
   return (
     <svg
-      width={size}
+      fill="none"
       height={size}
       viewBox="0 0 40 40"
-      fill="none"
+      width={size}
       xmlns="http://www.w3.org/2000/svg"
     >
       <circle
         cx="20"
         cy="20"
+        fill="#fff"
         r="19"
         stroke="#222"
         strokeWidth="2"
-        fill="#fff"
       />
       <path
         d={isLeft ? "M24 12L16 20L24 28" : "M16 12L24 20L16 28"}
         stroke="#222"
-        strokeWidth="2.5"
         strokeLinecap="round"
         strokeLinejoin="round"
+        strokeWidth="2.5"
       />
     </svg>
   );
