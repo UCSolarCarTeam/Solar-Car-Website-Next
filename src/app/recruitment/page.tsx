5f1b51a38e0bf353c61450a00dba37e3fd0b7c0e
justin-phxm <113923596+justin-phxm@users.noreply.github.com>
2025-10-11 12:24:11 -0600

    fixed recruitment screen

--- src/app/recruitment/page.tsx
+++ src/app/recruitment/page.tsx
@@ -8,43 +8,45 @@ import Footer from "@/app/_components/Footer";
 import Loader from "@/app/_components/Loader";
 import Navbar from "@/app/_components/Navbar";
 import styles from "@/app/recruitment/index.module.scss";
-import { type RouterOutputs, trpc } from "@/trpc/react";
-
-type RecruitmentForm = RouterOutputs["fe"]["getRecruitment"][0];
+import { trpc } from "@/trpc/react";
 
 const Recruitment = () => {
   const { data: recruitmentForms, isFetching } =
     trpc.fe.getRecruitment.useQuery();
 
   const RELEASE_DATE = new Date("2025-08-23T00:00:00");
-
+  const isRecruitmentClosed = RELEASE_DATE < new Date();
   return (
     <>
-      {true && <Loader isLoading={true} />}
       <main className={styles.main}>
         <>
           <Navbar />
           <div className={styles.container}>
             <div className={styles.pageHeading}>Applications</div>
-            <div className={styles.applicationDescription}>
-              Applications close on September 13th.
-            </div>
+            {!isRecruitmentClosed && (
+              <div className={styles.applicationDescription}>
+                Applications close on September 13th.
+              </div>
+            )}
             <div className={styles.applicationContainer}>
               {isFetching ? (
                 <Loader isLoading={isFetching} />
-              ) : RELEASE_DATE >= new Date() ? (
+              ) : isRecruitmentClosed ? (
                 <div className={styles.application}>
                   <div className={styles.applicationDescription}>
                     The current application period for the Calgary Solar Car
                     Team is now closed. We’ll be accepting new applications in
-                    September. If you’re excited about renewable energy,
+                    the next semester. If you’re excited about renewable energy,
                     engineering innovation, and working with a passionate
                     student team to build solar-powered vehicles, we encourage
-                    you to apply then!
+                    you to keep an eye on our{" "}
+                    <Link href="https://www.linkedin.com/company/university-of-calgary-solar-car-team">
+                      LinkedIn!
+                    </Link>
                   </div>
                 </div>
               ) : (
-                recruitmentForms?.map((form: RecruitmentForm) => (
+                recruitmentForms?.map((form) => (
                   <div className={styles.application} key={form.header}>
                     <div className={styles.applicationTitle}>{form.header}</div>
                     <div className={styles.applicationDescription}>
