b3ec86b896397300bf89928de6430ac555c0daa1
Justin Pham <jpham@mawer.com>
2026-02-08 02:53:03 -0700

    refactor: update footer component to use mapped social links and contact information

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -20,7 +20,72 @@ const dmSans = DM_Sans({
 
 const cx = classNames.bind(styles);
 
+const SOCIAL_LINKS = [
+  {
+    href: "https://www.facebook.com/UofCSolarTeam",
+    icon: Facebook,
+    label: "Facebook",
+  },
+  {
+    href: "https://x.com/uofcsolarcar",
+    icon: Twitter,
+    label: "Twitter",
+  },
+  {
+    href: "https://www.instagram.com/uofc_solarcar",
+    icon: Instagram,
+    label: "Instagram",
+  },
+  {
+    href: "https://www.linkedin.com/company/university-of-calgary-solar-car-team",
+    icon: Linkedin,
+    label: "LinkedIn",
+    props: { height: 26, width: 28 },
+  },
+  {
+    href: "https://www.youtube.com/user/calgarysolarcar",
+    icon: Youtube,
+    label: "YouTube",
+  },
+] as const;
+
+const CONTACT_INFO = [
+  {
+    href: "mailto:communications@calgarysolarcar.ca",
+    label: "communications@calgarysolarcar.ca",
+    type: "link" as const,
+  },
+  {
+    href: "mailto:sponsorship@calgarysolarcar.ca",
+    label: "sponsorship@calgarysolarcar.ca",
+    type: "link" as const,
+  },
+  {
+    label: "ENC 36, Schulich School of Engineering",
+    type: "text" as const,
+  },
+  {
+    label: "2500 University Dr NW",
+    type: "text" as const,
+  },
+  {
+    label: "Calgary, AB T2N 1N4",
+    type: "text" as const,
+  },
+] as const;
+
+const RESOURCE_LINKS = [
+  { href: "/", label: "Homepage" },
+  { href: "/cars", label: "Cars" },
+  { href: "/team", label: "Team" },
+  { href: "/support-us", label: "Support Us" },
+  { href: "/sponsors", label: "Sponsors" },
+  { href: "/portal/sign-in", label: "Team Portal" },
+] as const;
+
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <div className={styles.container}>
       <div className={cx(dmSans.className, styles.footer)}>
@@ -38,48 +103,41 @@ const Footer = () => {
           </div>
           <span>Follow us on our Social Media</span>
           <div className={styles.iconGrid}>
-            <Link href="https://www.facebook.com/UofCSolarTeam">
-              <Facebook />
-            </Link>
-            <Link href="https://x.com/uofcsolarcar">
-              <Twitter />
-            </Link>
-            <Link href="https://www.instagram.com/uofc_solarcar">
-              <Instagram />
-            </Link>
-            <Link href="https://www.linkedin.com/company/university-of-calgary-solar-car-team">
-              <Linkedin height={26} width={28} />
-            </Link>
-            <Link href="https://www.youtube.com/user/calgarysolarcar">
-              <Youtube />
-            </Link>
+            {SOCIAL_LINKS.map((social) => {
+              const Icon = social.icon;
+              const iconProps = "props" in social ? social.props : {};
+              return (
+                <Link href={social.href} key={social.label}>
+                  <Icon {...iconProps} />
+                </Link>
+              );
+            })}
           </div>
         </div>
         <div className={styles.gridContainer}>
           <div>Contact Information</div>
-          <Link href="mailto:communications@calgarysolarcar.ca">
-            communications@calgarysolarcar.ca
-          </Link>
-          <Link href="mailto:sponsorship@calgarysolarcar.ca">
-            sponsorship@calgarysolarcar.ca
-          </Link>
-          <span>ENC 36, Schulich School of Engineering</span>
-          <span>2500 University Dr NW</span>
-          <span>Calgary, AB T2N 1N4</span>
+          {CONTACT_INFO.map((item, index) =>
+            item.type === "link" ? (
+              <Link href={item.href} key={item.label}>
+                {item.label}
+              </Link>
+            ) : (
+              <span key={index}>{item.label}</span>
+            ),
+          )}
         </div>
         <div className={styles.gridContainer}>
           <div>Resources</div>
-          <Link href="/">Homepage</Link>
-          <Link href="/cars">Cars</Link>
-          <Link href="/team">Team</Link>
-          <Link href="/support-us">Support Us</Link>
-          <Link href="/sponsors">Sponsors</Link>
-          <Link href="/portal/sign-in">Team Portal</Link>
+          {RESOURCE_LINKS.map(({ href, label }) => (
+            <Link href={href} key={label} target="_blank">
+              {label}
+            </Link>
+          ))}
         </div>
       </div>
       <div className={styles.seperator} style={{ width: "80%" }} />
       <div className={styles.copyright}>
-        <span>© 2025 Calgary Solar Car</span>
+        <span>© {currentYear} Calgary Solar Car</span>
       </div>
     </div>
   );
