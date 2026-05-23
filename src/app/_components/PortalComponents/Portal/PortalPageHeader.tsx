f6bdd9218d3a596c21eb0fcbc725d708f5de6035
Justin Pham <justin.phamx4@gmail.com>
2026-05-23 13:28:36 -0600

    feat: enhance mobile navigation and responsiveness in PortalPageHeader and SponsorsTable

--- src/app/_components/PortalComponents/Portal/PortalPageHeader.tsx
+++ src/app/_components/PortalComponents/Portal/PortalPageHeader.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import logo from "public/assets/logo-center-black.png";
-import { memo } from "react";
+import { memo, useState } from "react";
 
 import styles from "@/app/_components/PortalComponents/Portal/index.module.scss";
+import Chevron from "@/app/_components/svgs/Chevron";
+import useViewport from "@/app/_hooks/useViewport";
 import { PortalNavigationLinks, adminClerkRoles } from "@/app/_types";
 import { type AdminRoles } from "@/server/api/routers/portal";
 import { UserButton } from "@clerk/nextjs";
@@ -20,66 +24,159 @@ const PortalPageHeader = ({
   currentUser,
   setCurrentPage,
 }: PortalPageHeaderProps) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const { width } = useViewport();
+  const isMobile = width !== undefined && width <= 640;
+
+  const handleMenuItemClick = (page: PortalNavigationLinks) => {
+    setCurrentPage(page);
+    setIsMenuOpen(false);
+  };
+
   return (
-    <header className={styles.portalPageHeaderLayout}>
-      <div className={styles.portalPageHeader}>
-        <Link href="/">
-          <Image alt="navlogo" height={48} src={logo} width={48} />
-        </Link>
-        {adminClerkRoles.includes(
+    <>
+      <header className={styles.portalPageHeaderLayout}>
+        <div className={styles.portalPageHeader}>
+          <Link href="/">
+            <Image alt="navlogo" height={48} src={logo} width={48} />
+          </Link>
+          {adminClerkRoles.includes(
+            (currentUser?.publicMetadata.role as AdminRoles) ?? "",
+          ) && (
+            <>
+              {!isMobile && (
+                <div className={styles.desktopNavLinks}>
+                  <div
+                    className={`${currentPage === "team" ? styles.active : ""}`}
+                    onClick={() =>
+                      handleMenuItemClick(PortalNavigationLinks.Team)
+                    }
+                  >
+                    Team
+                  </div>
+                  <div
+                    className={`${currentPage === "users" ? styles.active : ""}`}
+                    onClick={() =>
+                      handleMenuItemClick(PortalNavigationLinks.Users)
+                    }
+                  >
+                    Users
+                  </div>
+                  <div
+                    className={`${currentPage === "sponsors" ? styles.active : ""}`}
+                    onClick={() =>
+                      handleMenuItemClick(PortalNavigationLinks.Sponsors)
+                    }
+                  >
+                    Sponsors
+                  </div>
+                  <div
+                    className={`${currentPage === "invitations" ? styles.active : ""}`}
+                    onClick={() =>
+                      handleMenuItemClick(PortalNavigationLinks.Invitations)
+                    }
+                  >
+                    Invitations
+                  </div>
+                  <div
+                    className={`${currentPage === "recruitment" ? styles.active : ""}`}
+                    onClick={() =>
+                      handleMenuItemClick(PortalNavigationLinks.Recruitment)
+                    }
+                  >
+                    Recruitment
+                  </div>
+                  <div
+                    className={`${currentPage === "our-work" ? styles.active : ""}`}
+                    onClick={() =>
+                      handleMenuItemClick(PortalNavigationLinks.OurWork)
+                    }
+                  >
+                    Our Work
+                  </div>
+                  <div
+                    className={`${currentPage === "alumni" ? styles.active : ""}`}
+                    onClick={() =>
+                      handleMenuItemClick(PortalNavigationLinks.Alumni)
+                    }
+                  >
+                    Alumni
+                  </div>
+                </div>
+              )}
+              {isMobile && (
+                <div
+                  className={styles.hamburgerIcon}
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                >
+                  <Chevron className={styles.chevronIcon} />
+                </div>
+              )}
+            </>
+          )}
+        </div>
+        <div className={styles.profilePicture}>
+          {currentUser?.username}
+          <UserButton />
+        </div>
+      </header>
+      {isMobile &&
+        isMenuOpen &&
+        adminClerkRoles.includes(
           (currentUser?.publicMetadata.role as AdminRoles) ?? "",
         ) && (
-          <div>
+          <div className={styles.mobileMenuDropdown}>
             <div
               className={`${currentPage === "team" ? styles.active : ""}`}
-              onClick={() => setCurrentPage(PortalNavigationLinks.Team)}
+              onClick={() => handleMenuItemClick(PortalNavigationLinks.Team)}
             >
               Team
             </div>
             <div
               className={`${currentPage === "users" ? styles.active : ""}`}
-              onClick={() => setCurrentPage(PortalNavigationLinks.Users)}
+              onClick={() => handleMenuItemClick(PortalNavigationLinks.Users)}
             >
               Users
             </div>
             <div
               className={`${currentPage === "sponsors" ? styles.active : ""}`}
-              onClick={() => setCurrentPage(PortalNavigationLinks.Sponsors)}
+              onClick={() =>
+                handleMenuItemClick(PortalNavigationLinks.Sponsors)
+              }
             >
               Sponsors
             </div>
             <div
               className={`${currentPage === "invitations" ? styles.active : ""}`}
-              onClick={() => setCurrentPage(PortalNavigationLinks.Invitations)}
+              onClick={() =>
+                handleMenuItemClick(PortalNavigationLinks.Invitations)
+              }
             >
               Invitations
             </div>
             <div
               className={`${currentPage === "recruitment" ? styles.active : ""}`}
-              onClick={() => setCurrentPage(PortalNavigationLinks.Recruitment)}
+              onClick={() =>
+                handleMenuItemClick(PortalNavigationLinks.Recruitment)
+              }
             >
               Recruitment
             </div>
             <div
               className={`${currentPage === "our-work" ? styles.active : ""}`}
-              onClick={() => setCurrentPage(PortalNavigationLinks.OurWork)}
+              onClick={() => handleMenuItemClick(PortalNavigationLinks.OurWork)}
             >
               Our Work
             </div>
             <div
               className={`${currentPage === "alumni" ? styles.active : ""}`}
-              onClick={() => setCurrentPage(PortalNavigationLinks.Alumni)}
+              onClick={() => handleMenuItemClick(PortalNavigationLinks.Alumni)}
             >
               Alumni
             </div>
           </div>
         )}
-      </div>
-      <div className={styles.profilePicture}>
-        {currentUser?.username}
-        <UserButton />
-      </div>
-    </header>
+    </>
   );
 };
 
