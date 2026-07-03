6f9048990bac3b47fb121649ddb3ede31aa83035
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2026-07-03 12:10:47 -0600

    statically render: /support-us, /, /_not-found, /cars (#61)

--- src/app/portal/layout.tsx
+++ src/app/portal/layout.tsx
@@ -1,12 +1,23 @@
+import { ClerkProvider } from "@clerk/nextjs";
+
+// export meta
+export const metadata = {
+  title: "Portal - Calgary Solar Car",
+};
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <>
-      <title>Calgary Solar Car - Portal</title>
+    <ClerkProvider
+      dynamic
+      signInForceRedirectUrl="/portal"
+      signInUrl="/portal/sign-in"
+      signUpForceRedirectUrl="/portal"
+      signUpUrl="/portal/sign-up"
+    >
       {children}
-    </>
+    </ClerkProvider>
   );
 }
