f0a38a4a25bcdb2125c44e27ce3c8251bdca685e
Justin Pham <113923596+justin-phxm@users.noreply.github.com>
2026-07-25 12:01:04 -0600

    refactor: update framer-motion import style and remove "use client" directive from multiple components (#80)

--- src/app/sponsors/SponsorCards.tsx
+++ src/app/sponsors/SponsorCards.tsx
@@ -1,6 +1,4 @@
-"use client";
-
-import { motion } from "framer-motion";
+import * as motion from "framer-motion/client";
 import Image from "next/image";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
